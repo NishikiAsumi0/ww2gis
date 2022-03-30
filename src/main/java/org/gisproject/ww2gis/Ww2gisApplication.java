27d676aa2be9dc86a69a6fcb558302730553989e
Zhang Yunpeng <zhangyunpeng0127@gmail.com>
2022-03-30 10:23:16 +0800

    initial commit

---
+++ src/main/java/org/gisproject/ww2gis/Ww2gisApplication.java
@@ -0,0 +1,15 @@
+package org.gisproject.ww2gis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ww2gisApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Ww2gisApplication.class, args);
+    }
+
+}
