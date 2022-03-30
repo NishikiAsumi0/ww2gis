a1e280b76a372e4203cd1ea60d0ce4a9048bf1cb
Zhang Yunpeng <zhangyunpeng0127@gmail.com>
2022-03-30 10:40:33 +0800

    initial commit

---
+++ src/main/java/org/gisproject/ww2gis/Constant.java
@@ -0,0 +1,12 @@
+package org.gisproject.ww2gis;
+
+/**
+ * @author Zhang Yunpeng
+ * @date 2022/3/30
+ */
+public final class Constant
+{
+    public static final String NAME = "World War II GIS";
+    public static final String NAME_SHORT = "WW2GIS";
+    public static final String VERSION = "0.0.0";
+}
